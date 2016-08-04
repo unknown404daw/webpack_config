1b8ebd5a5f1de12030981e7bea917d5c83a125df
villapilla <dvillaluenga@gmail.com>
2016-08-04 11:16:55 +0200

    Update webpack.config.js
    config complete for js and sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,81 @@
-'use strict';
+/// <binding BeforeBuild='Run - Development' />
+"use strict";
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('./app/node_modules/webpack'),
+    ExtractTextPlugin = require('./app/node_modules/extract-text-webpack-plugin');
 
 module.exports = {
     entry: {
-        js: './src/app.js',
-        css: './style/css.js'
+        main: './app/src/app.ts',
+        css: './app/styles/style.js'
     },
     output: {
-        path: './bin',
-        filename: 'bundle.js'
+        filename: 'build.js',
+        path: './app/dist',
+        publicPath: '/'
     },
-    module: {
-        loaders: [
-            { 
-                test: /\.scss$/i, 
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-            }
-        ]
+    devtool: 'source-map',
+    resolve: {
+        root: __dirname,
+        extensions: ['', '.ts', '.js', '.json']
+    },
+    resolveLoader: {
+        modulesDirectories: ["./app/node_modules"]
     },
     plugins: [
-        new ExtractTextPlugin("style.css", {
+        //new webpack.optimize.UglifyJsPlugin(
+        //    {
+        //        warning: false,
+        //        mangle: true,
+        //        comments: false
+        //    }
+        //),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'window.jquery': 'jquery',
+            toastr: 'toastr',
+            bootstrap: 'bootstrap'
+        }),
+        new ExtractTextPlugin('style.min.css', {
             allChunks: true
         })
-    ]
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+                //loader: ExtractTextPlugin.extract('style-loader', 'css-loader?minimize')
+            },
+            {
+                test: /\.scss$/i,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+                //loader: ExtractTextPlugin.extract('style-loader', 'css-loader?minimize!sass-loader')
+            }, {
+                test: /\.html$/,
+                exclude: /app\node_modules/,
+                loader: 'raw'
+            }, {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            }, {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            }, {
+                test: '\.jpg$',
+                exclude: /app\node_modules/,
+                loader: 'file'
+            }, {
+                test: '\.png$',
+                exclude: /app\node_modules/,
+                loader: 'url'
+            }
+        ]
+    }
 };
